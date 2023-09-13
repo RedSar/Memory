0f4a42095e7ab41922b7ad1c7b086b0038572843
RedSar <guerchal.radouane@gmail.com>
2023-09-13 14:24:15 +0100

    create 4x2 grid

--- app/src/main/java/com/rsmi/memory/MyMemoryBoardAdapter.kt
+++ app/src/main/java/com/rsmi/memory/MyMemoryBoardAdapter.kt
@@ -1,15 +1,37 @@
 package com.rsmi.memory
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.core.view.setMargins
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MyMemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MyMemoryBoardAdapter.ViewHolder>() {
+
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MyMemoryBoardAdapter"
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val cardWith: Int = parent.width/2 - 2 * MARGIN_SIZE
+        val cardHeight: Int = parent.height/4 - 2 * MARGIN_SIZE
+        val cardDimension: Int = min(cardHeight,cardWith)
+
         val itemView:View  = LayoutInflater.from(context).inflate(R.layout.memory_card,parent, false)
+
+        val cardLayoutParams: MarginLayoutParams = itemView.findViewById<CardView>(R.id.cardView).layoutParams as MarginLayoutParams
+        cardLayoutParams.height = cardDimension
+        cardLayoutParams.width = cardDimension
+        cardLayoutParams.setMargins(MARGIN_SIZE)
+
         return ViewHolder(itemView)
     }
 
@@ -19,8 +41,11 @@ class MyMemoryBoardAdapter(private val context: Context, private val numPieces: 
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position: Int) {
-
+            imageButton.setOnClickListener{
+                Log.i(TAG, "Image clicked on position: $position")
+            }
         }
 
     }
---
+++ app/src/main/java/com/rsmi/memory/models/BroadSize.kt
@@ -0,0 +1,5 @@
+package com.rsmi.memory.models
+
+enum class BroadSize {
+
+}
